137b3960fb8051d6b715d31682557f207b57aa80
赵梓江 <qwe123.123.123@foxmail.com>
2025-01-17 11:48:48 +0800

    fix: 修复嵌入模块错误

--- src/client/embedding.py
+++ src/client/embedding.py
@@ -1,3 +1,4 @@
+import pickle
 from conf.config import config
 from sentence_transformers import SentenceTransformer
 from src.init.init import redis_client
@@ -8,13 +9,13 @@ embedding_cache = {}
 
 class EmbeddingClient:
     def __init__(self):
-        self.model_names = list(config.embedding_model.keys())
+        self.model_names = list(config["embedding_model"].keys())
         self.models = {}
         for model_name in self.model_names:
             self.load_model(model_name)
     def load_model(self, model_name):
-        self.models[model_name] = SentenceTransformer(config.embedding_model[model_name]["path"])
-    def get_embedding(self, texts, model_name):
+        self.models[model_name] = SentenceTransformer(config["embedding_model"][model_name]["path"])
+    def get_embedding(self, texts, model_name="bge-small-zh-v1.5"):
         embedding_keys = [f"embedding:{model_name}:{text}" for text in texts]
         embedding_list = []
 
@@ -25,21 +26,26 @@ class EmbeddingClient:
                 embedding_list.append(None)
 
         try:
-            index_list, key_list = zip(*[(i, embedding_keys[i]) for i, embedding in enumerate(embedding_list) if embedding is None])
-            if len(key_list) > 0:
+            missing_data = [(i, embedding_keys[i]) for i, embedding in enumerate(embedding_list) if embedding is None]
+            if missing_data:
+                index_list, key_list = zip(*missing_data)
                 embeddings = redis_client.mget(key_list)
                 for i in range(len(key_list)):
-                    embedding_list[index_list[i]] = embeddings[i]
-                    embedding_cache[key_list[i]] = embeddings[i]
+                    if embeddings[i]:
+                        embeddings_loaded = pickle.loads(embeddings[i])
+                        embedding_list[index_list[i]] = embeddings_loaded
+                        embedding_cache[key_list[i]] = embeddings_loaded
         except Exception as e:
-            logger.warning("redis读取嵌入缓存失败")
+            logger.warning(f"redis读取嵌入缓存失败：{e}")
 
-        index_list, key_list, text_list = zip(*[(i, embedding_keys[i], texts[i]) for i, embedding in enumerate(embedding_list) if embedding is None])
-        if len(text_list) > 0:
+        missing_data = [(i, embedding_keys[i], texts[i]) for i, embedding in enumerate(embedding_list) if embedding is None]
+        if missing_data:
+            index_list, key_list, text_list = zip(*missing_data)
             embeddings = self.models[model_name].encode(text_list, normalize_embeddings=True)
             for i in range(len(text_list)):
                 embedding_list[index_list[i]] = embeddings[i]
                 embedding_cache[key_list[i]] = embeddings[i]
-            redis_client.mset({key_list[i]: embeddings[i] for i in range(len(key_list))})
+                redis_client.set(key_list[i], pickle.dumps(embeddings[i]), ex=60*60*24*7)
+            # redis_client.mset({key_list[i]: pickle.dumps(embeddings[i]) for i in range(len(key_list))})
 
         return embedding_list
