8f290639e781e6649f959f7c4ead91ca6776b2a5
赵梓江 <qwe123.123.123@foxmail.com>
2025-01-18 00:16:25 +0800

    feat: 添加模型自动下载功能并扩展模型支持
    - 新增download.py模块，实现Hugging Face模型自动下载功能
    - 在config.yaml中添加新的嵌入模型和LLM模型配置
    - 修改init.py以在应用初始化时自动下载缺失模型
    - 更新requirements.txt，添加huggingface-hub依赖

--- src/init/init.py
+++ src/init/init.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 import os
-from src.init.sql_init import create_vdb_info_table
-from src.client.embedding import EmbeddingClient
+from src.init.download import download_hf_model
+from conf.config import config
+
+
+models = {**config["embedding_model"], **config["LLM"]}
+hf_name_list = [models[model]["hf_name"] for model in models]
+model_path_list = [models[model]["path"] for model in models]
+
+for i in range(len(hf_name_list)):
+    if not os.path.exists(model_path_list[i]):
+        download_hf_model(hf_name_list[i], model_path_list[i])
 
 
 app = FastAPI()
@@ -11,8 +20,12 @@ if not os.path.exists("database"):
 if not os.path.exists("files"):
     os.mkdir("files")
 
+
+from src.init.sql_init import create_vdb_info_table
+from src.client.embedding import EmbeddingClient
+
+
 # 表初始化
 create_vdb_info_table("database/sqlite.db")
-
 
 EmbedClient = EmbeddingClient()
